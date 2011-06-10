211d7cade24e5ab691c064d4582df9a65c30ed68
yuyeon.oh <yuyeon.oh@samsung.com>
2011-06-10 21:14:15 +0900

    start samsung virtual overlay driver from scratch
    Change-Id: I36c42c771d71fac4cd3864fcc0c1d644c72310d2

---
+++ drivers/media/video/svo.c
@@ -0,0 +1,12 @@
+/*
+ * Samsung virtual overlay driver.
+ *
+ * Copyright (c) 2011 Samsung Electronics Co., Ltd All Rights Reserved
+ *
+ * Authors:
+ *  Yuyeon Oh   <yuyeon.oh@samsung.com>
+ *
+ * This work is licensed under the terms of the GNU GPL, version 2 or later.
+ * See the COPYING file in the top-level directory.
+ */
+ 
