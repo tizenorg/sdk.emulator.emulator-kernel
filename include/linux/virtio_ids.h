f42898265a44016e911a16bcc8ddb1af55b763b7
sungmin ha <sungmin82.ha@samsung.com>
2013-03-20 12:22:51 +0900

    hwkey: added virtio hwkey driver
    improve hwkey mapping
    
    Signed-off-by: Sungmin Ha <sungmin82.ha@samsung.com>

--- include/linux/virtio_ids.h
+++ include/linux/virtio_ids.h
@@ -44,6 +44,7 @@
 #define VIRTIO_ID_TOUCHSCREEN	11 /* virtio touchscreen */
 #define VIRTIO_ID_KEYBOARD	12 /* virtio keyboard */
 #define VIRTIO_ID_ESM		13 /* virtio ESM */
+#define VIRTIO_ID_HWKEY		14 /* virtio hwkey */
 #endif
 
 #endif /* _LINUX_VIRTIO_IDS_H */
