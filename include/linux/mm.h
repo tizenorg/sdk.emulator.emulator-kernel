e234d1ad554defb4bda83cf39f1d505633984912
Stanislav Vorobiov <s.vorobiov@samsung.com>
2013-07-11 10:39:15 +0400

    mm: export do_mmap_pgoff
    For VIGS DRM we can't use 'do_mmap' (like in i915, exynos and others)
    because 'do_mmap' takes only 32-bit offsets, but TTM uses 64-bit
    offsets.
    'do_mmap_pgoff' was exported prior to 3.4 and it's available
    in 3.5, but for some reason it's static in 3.4

--- include/linux/mm.h
+++ include/linux/mm.h
@@ -1396,6 +1396,9 @@ extern unsigned long get_unmapped_area(struct file *, unsigned long, unsigned lo
 extern unsigned long mmap_region(struct file *file, unsigned long addr,
 	unsigned long len, unsigned long flags,
 	vm_flags_t vm_flags, unsigned long pgoff);
+extern unsigned long do_mmap_pgoff(struct file *file, unsigned long addr,
+        unsigned long len, unsigned long prot,
+        unsigned long flags, unsigned long pgoff);
 extern unsigned long do_mmap(struct file *, unsigned long,
         unsigned long, unsigned long,
         unsigned long, unsigned long);
