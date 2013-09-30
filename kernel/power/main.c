6ff494c4dab9d74306f99d0283e901abe9791ad9
SeokYeon Hwang <syeon.hwang@samsung.com>
2013-09-30 16:25:55 +0900

    Enable suspend on kernel
    Change-Id: Ice3e8c1ce10ea660f612d1c7ab58d87b30ba958f
    Signed-off-by: SeokYeon Hwang <syeon.hwang@samsung.com>

--- kernel/power/main.c
+++ kernel/power/main.c
@@ -272,8 +272,6 @@ static ssize_t state_show(struct kobject *kobj, struct kobj_attribute *attr,
 static ssize_t state_store(struct kobject *kobj, struct kobj_attribute *attr,
 			   const char *buf, size_t n)
 {
-#if 0 // do not allow suspend in emulator
-
 #ifdef CONFIG_SUSPEND
 	suspend_state_t state = PM_SUSPEND_STANDBY;
 	const char * const *s;
@@ -302,10 +300,6 @@ static ssize_t state_store(struct kobject *kobj, struct kobj_attribute *attr,
 
  Exit:
 	return error ? error : n;
-#else
-
-	return n;
-#endif // do not allow suspend in emulator
 }
 
 power_attr(state);
